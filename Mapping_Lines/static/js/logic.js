cf575a852148d89df9a47ff941654095bf0d5077
Nehla A <namin.general@gmail.com>
2022-07-03 15:54:03 -0400

    Pushing files for Mapping_Multiple_Maps

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -17,10 +17,6 @@ L.polyline(line, {
     color: "yellow"
   }).addTo(map);
 
-// Get data from cities.js
-let cityData = cities;
-
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
